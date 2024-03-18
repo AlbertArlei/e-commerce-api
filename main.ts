f210492fc465f1c857bb68ac51aff3821e707936
Albert Arlei <albertarlei364@gmail.com>
2024-03-17 22:05:39 -0300

    docker config

--- main.ts
+++ main.ts
@@ -5,11 +5,11 @@ import { router } from "./router";
 const app = express();
 
 app.use(cors({
-    origin:'10.0.0.134:5173',
+    origin:'frontend:5173',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE'
 }));
 app.use(router);
 
-app.listen(8000, '10.0.0.134', () => {
+app.listen(8000, () => {
     console.log('server is running...');
 });
